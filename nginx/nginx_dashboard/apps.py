6613ed2e4e62e55f764b8dc1e293b5963c25d59a
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-03-06 19:35:14 +0000

    Add files

---
+++ nginx/nginx_dashboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NginxDashboardConfig(AppConfig):
+    name = "nginx_dashboard"
---
+++ nginx/users/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from urllib3 import request
+
+# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("nginx_dashboard:index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials."
+            })
+        
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("users:login"))
